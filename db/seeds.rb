aee1d31b67d002ac21ae7f626f5ac35de7ebebdc
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-08-06 00:52:32 -0400

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,22 @@ Comment.destroy_all
 Morgan = User.create!(username: "morganrote", password_digest: "peanutbutter&jelly")
 Sarah = User.create!(username: "sarahlavatai", password_digest: "milk&cookies")
 Leanne = User.create!(username: "leannenaramore", password_digest: "crunchygranola")
+Lindsey = User.create!(username: "lindseydortch", password_digest: "ashevilleawesome")
+Grayson Roberts = User.create!(username: "graysonroberts", password_digest: "iamgraysonroberts")
 
 Atlanta = Location.create!(city_name: "Atlanta", state_name: "Georgia", country_name: "United States")
-San_Francisco = Location.create!(city_name: "San Francisco", state_name: "California", country_name: "United States")
+San_Francisco = Location.create!(city_name: "San Francisco", state_name: "California")
+Asheville = Location.create!(city_name: "Asheville", state_name: "North Carolina", country_name: "United States")
 
 Dr_Bombays = Recommendation.create!(recommended_place: "Dr. Bombay's Underwater Tea Party", user_id: Sarah.id, location_id: Atlanta.id, body: "Looking for a great place to post up and do some work, to read a book, or to catch up with a friend over a pot of tea? Check out Dr. Bombay's Underwater Tea Party!")
+Piedmont_Park = Recommendation.create!(recommended_place: "Piedmont Park", user_id: Leanne.id, location_id: Atlanta.id, body: "Piedmont Park is a 189-acre park in the heart of Midtown. It's an awesome spot to hang out and relax. The park is walking distance to Trader Joe's as well as many restaurants - bring a blanket and a picnic! There is also a dog park as well as an outdoor pool, and there are frequently concerts and festivals at the park in the spring, fall, and summer.")
 Tea_Garden = Recommendation.create!(recommended_place: "Japanese Tea Garden", user_id: Morgan.id, location_id: San_Francisco.id, body: "The Japanese Tea Garden, a five acre garden within Golden Gate State Park is an awesome spot. Built in 1894 as part of the World's Fair, it is the oldest public tea garden in the United States.")
+Point_Reyes = Recommendation.create!(recommended_place: "Point Reyes National Seashore", user_id: Lindsey.id, location_id: San_Francisco.id, body: "Point Reyes is located about 30 miles north of San Francisco, but it is well worth the drive. If the view's California's coast don't take your breath away, perhaps the elk or the elephant seals will.")
+Blue_Ridge_Parkway = Recommendation.create!(recommended_place: "Blue Ridge Parkway", user_id: Leanne.id, location_id: Asheville.id, body: "If your trip involves driving into Asheville, try to make part of the trip on the Blue Ridge Parkway. Don't plan on getting anywhere fast (the speed limit is 45mph), but do plan to take an some incredible views of the Blue Ridge Mountains. The parkway runs for 469 miles through 29 Virginia and North Carolina counties, mostly along teh Blue Ridge, a mountain chain that is part of the Appalachian Mountains.")
+Edible_Park = Recommendation.create!(recommended_place: "George Washington Carver Edible Park", user_id: Morgan.id, location_id: Asheville.id, body: "This is truly a hidden gem! This park is tucked away in a residential neighborhood less than one mile from downtown Asheville and it contains over forthy fruit and nut trees, vines, and shrubs. Visitors are encouraged to explore the park and sample its fruits and nuts.")
+Jack_of_the_Wood = Recommendation.create!(recommended_place: "Jack of the Wood", user_id: Lindsey.id, location_id: Asheville.id, body: "Asheville is teeming with breweries, so it's touch to pick a favorite, but Jack of the Wood is definitely in my top five. Check out the live bluegrass jam sessions that take place every Thursday.")
+Sweet_Peas = Recommendation.create!(recommended_place: "Sweetpeas", user_id: Grayson.id, location_id: Asheville.id, body: "This place is a sweet deal. The hostel is clean and well-run, and it is in the heart of downtown Asheville. The people are cool and the prices run from about $25-$60/night depending on whether you have a private or shared room.")
+
 
 Dr_Bombays_comment1 = Comment.create!(user_id: Morgan.id, recommendation_id: Dr_Bombays.id, body: "Dr. Bombay's is my favorite coffee shop in Atlanta. It's located in Candler Park, there's free wi-fi, and the walls are lined with built in bookcases.")
 Dr_Bombays_comment2 = Comment.create!(user_id: Leanne.id, recommendation_id: Dr_Bombays.id, body: "The walls at Dr. Bombay's are lined with books that are for sale for just a dollar each. All proceeds from book sales benefit scholarship opportunities for young women in India.")
