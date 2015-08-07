9dc91302c55de6ba0409946f921795aa3b26aa1f
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-08-06 22:47:23 -0400

    removed password column from user table

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150806220357) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    #t.string   "password"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
