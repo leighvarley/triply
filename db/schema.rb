1802d46ffb9d2c3b9a4424267ae40078a75504df
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-08-07 14:36:01 -0400

    trying to fix data issues

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20150806220357) do
     #t.string   "password"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
