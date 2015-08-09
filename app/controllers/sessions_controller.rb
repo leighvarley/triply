bbd50e5f295e1da6c0bb9bfc0d3b8a13b41fbcb0
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-08-09 16:53:20 -0400

    sessions set up; worked on layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,29 @@
 class SessionsController < ApplicationController
+#Rather than using a model to persist data, session uses cookies to authenticate
 
+  #visiting the login page renders a form for a new session
   def new
   end
 
+  #logging in creates a session (post request to create action)
   def create
+    #params[:session][:email] is the submitted email
     user = User.find_by(email: params[:session][:email].downcase)
-    #&& statement determines whether or not user is valid
+    #&& statement determines whether or not user is valid by comparing what is entered in the form with the database to see if there is a match
+    #params[:session][:password] is the submitted password
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
+      #Flash persists for one request, but re-rendering a template (instead of redirecting) doesn't count as a request, so the msg will show up on the next page visited - correct this with .now
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
+  #logging out destroys the session (delete request to destroy action)
   def destroy
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+  # Logs in the given user
+  def log_in(user)
+    #puts temporary cookie on user's browser w/encrypted version of user's id so that the id can be retrieved on other pages - expires when browser is closed
+    session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # A user is logged in if there's a current user in the session (if current_user is not nil)
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
