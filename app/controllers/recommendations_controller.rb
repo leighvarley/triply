1d15b7a875f11246cf02e28fe3ed7d6f4752589b
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-08-09 23:50:51 -0400

    added style

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,6 +1,6 @@
 class RecommendationsController < ApplicationController
 
-before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+#before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
 #before_action :authenticate, only: [:new, :create, :edit, :update, :destroy]
 
   #index
